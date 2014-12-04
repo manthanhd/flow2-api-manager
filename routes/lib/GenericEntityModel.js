ad5901eb9f1e1b5c99143cf8e9c9f439f0bc577a
Manthan Dave <manthanhd@live.com>
2014-12-04 23:31:20 +0000

    Added new attribute "active". This can be used to active/deactivate Entities.

--- routes/lib/GenericEntityModel.js
+++ routes/lib/GenericEntityModel.js
@@ -2,6 +2,7 @@ var mongoose = require("mongoose");
 
 var entitySchema = mongoose.Schema({
     name: String,
+    active: Boolean,
     properties: [{
         name: String,
         value: String,
