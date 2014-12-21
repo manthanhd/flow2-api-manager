583ff2384592e33d604b8d542c08210ebb1db399
Manthan Dave <manthanhd@live.com>
2014-12-21 22:22:20 +0000

    Model for Role added.

---
+++ routes/lib/sec/RoleModel.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+var roleSchema = mongoose.Schema({
+  name: String,
+  description: String,
+  context: String,  // entity or user or role
+  allowsOperation: [String], // Array of c, r, u, d, a (a/assign only for context role and user.);
+  allowedAuthTypes: [String]  // UsernameAndPassword and/or OAuth
+});
+
+var roleDBName = "FLOW2_Roles";
+var RoleModel = mongoose.model(roleDBName, roleSchema, roleDBName);
+
+module.exports = RoleModel;
