986e2bc7804fd9d0c0f3188dc5f8ab1f3686200b
Manthan Dave <manthanhd@live.com>
2015-02-01 23:09:10 +0000

    Shared module defined. RequestService added for handling low level request parameters.

---
+++ public/javascripts/angular-app/shared/services/RequestService.js
@@ -0,0 +1,14 @@
+var sharedModule = angular.module("Shared");
+sharedModule.service("RequestService", function($http) {
+    this.getEntityList = function(onSuccess, onFailure) {
+        $http.get("/entity").success(function(data, statusCode) {
+            onSuccess(data, statusCode);
+        }).error(function(data, statusCode) {
+            if(statusCode == 403 && data && data.errorCode == 403 && data.error == "LoginRequired") {
+                window.location.href = "/user/login";
+            } else {
+                onFailure(data, statusCode);
+            }
+        });
+    };
+});
