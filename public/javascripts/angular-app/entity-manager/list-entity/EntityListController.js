bf00df82ead8d13dd1d0948248fed600acafbbc3
Manthan Dave <manthanhd@live.com>
2015-02-01 23:05:07 +0000

    Now getting real entity list through API.

--- public/javascripts/angular-app/entity-manager/list-entity/EntityListController.js
+++ public/javascripts/angular-app/entity-manager/list-entity/EntityListController.js
@@ -1,4 +1,12 @@
 var entitiesModule = angular.module("Entities");
-entitiesModule.controller("EntityListController", function($scope, $http) {
+entitiesModule.controller("EntityListController", function($scope, $http, RequestService) {
+    RequestService.getEntityList(onEntityListSuccess, onEntityListFailure);
 
+    function onEntityListSuccess(data, statusCode) {
+        $scope.entityList = data.entityList;
+    };
+
+    function onEntityListFailure(data, statusCode) {
+
+    };
 });
