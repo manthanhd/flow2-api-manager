ebbfcea536b682a5c2176d3eaaa978974358fd74
Manthan Dave <manthanhd@live.com>
2015-02-08 11:48:21 +0000

    Clicking on Add Entity now transforms Entities title into a text field with a ok button enabling the user to add a new entity.

--- public/javascripts/angular-app/entity-manager/EntityController.js
+++ public/javascripts/angular-app/entity-manager/EntityController.js
@@ -1,4 +1,7 @@
 var entitiesModule = angular.module("Entities");
 entitiesModule.controller("EntityController", function($scope) {
-
+    $scope.showAddEntity = function() {
+        $scope.newEntity = {};
+        $scope.enableEntityAdd = true;
+    }
 });
