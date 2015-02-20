84adc61fc5771b8739b511fc1878d0d531712728
Manthan Dave <manthanhd@live.com>
2015-02-20 21:46:12 +0000

    Added validations on save entity action.

--- public/javascripts/angular-app/entity-manager/EntityController.js
+++ public/javascripts/angular-app/entity-manager/EntityController.js
@@ -22,6 +22,29 @@ entitiesModule.controller("EntityController", function($scope, RequestService) {
     }
 
     $scope.saveEntity = function() {
+        var alphaNumericUnderscoreRegex = new RegExp("^[A-Za-z0-9_]+$");
+        if(alphaNumericUnderscoreRegex.test($scope.newEntity.entityName) != true) {
+            toast("Entity name cannot contain spaces or special characters. A-Z, a-z, 0-9 and _ allowed.", 2000);
+            return;
+        } else if($scope.newEntity.properties.length == 0) {
+            toast("An entity must have at least 1 property.", 2000);
+            return;
+        }
+
+        // Thorough property validation.
+        for(var i = 0; i < $scope.newEntity.properties.length; i++) {
+            var property = $scope.newEntity.properties[i];
+            if(property.name == "") {
+                toast("Property name of property " + (i + 1) + " cannot be blank.", 2000);
+                return;
+            } else if(alphaNumericUnderscoreRegex.test(property.name) != true) {
+                toast(property.name + " is an invalid property name.", 2000);
+                return;
+            } else if(property.type != "string" && property.type != "number" && property.type != "date") {
+                toast(property.type + " is an invalid property value.", 2000);
+                return;
+            }
+        }
         $("#confirmAddModal").openModal();
     }
 
