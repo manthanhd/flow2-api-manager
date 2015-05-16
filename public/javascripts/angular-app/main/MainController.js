a9197bf9ab4bf1d50e3583e538504186302e6d12
Manthan Dave <manthanhd@live.com>
2015-05-16 11:25:36 +0100

    Added disabled class + whole li is clickable in sidebar.

--- public/javascripts/angular-app/main/MainController.js
+++ public/javascripts/angular-app/main/MainController.js
@@ -1,4 +1,14 @@
 var mainModule = angular.module("Main");
 mainModule.controller("MainController", function($scope) {
     $scope.currentSection = "entities";
+
+    $scope.goto = function(location) {
+        window.location.href = "#/".concat(location);
+        $('#slide-out').sideNav('hide');
+    }
+
+    $scope.gotoUrl = function(location) {
+        window.open(location, '_blank');
+        $('#slide-out').sideNav('hide');
+    }
 });
