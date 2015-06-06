042cebcf07c1acc9282045ba6ad744db78fefb60
Manthan Dave <manthanhd@live.com>
2015-06-06 18:20:34 +0100

    Model for api key storage added.

---
+++ routes/lib/sec/ApiKeyModel.js
@@ -0,0 +1,19 @@
+/**
+ * Created by manthanhd on 06/06/2015.
+ */
+var mongoose = require('mongoose');
+var hat = require("hat");
+
+var ObjectId = mongoose.Schema.Types.ObjectId;
+
+var apiKeySchema = mongoose.Schema({
+    userId: {type: ObjectId, index: true, required: true},
+    apiKey: {type: String, index: {unique: true}, required: true, default: hat()},
+    createdOn: {type: Date, default: Date.now},
+    permissions: [{ action: String, realm: String, actionableObject: String }]
+});
+
+var apiKeyDBName = "FLOW2_APIKEYS";
+var ApiKeyModel = mongoose.model(apiKeyDBName, apiKeySchema, apiKeyDBName);
+
+module.exports = ApiKeyModel;
